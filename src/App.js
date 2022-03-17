1a2630a6cc577ba8a5063b88b9a5af7314a44f96
Tom wallwin <tomwallwin@toms-mbp.home>
2022-03-17 15:12:34 +0000

    made no progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import cloudinary from "cloudinary";
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Upload a photo to cloudinary</header>
+      {cloudinary.openUploadWidget(
+        {
+          cloud_name: `${process.env.REACT_APP_NAME}`,
+          upload_preset: `${process.env.REACT_APP_UPLOAD_PRESET}`,
+        },
+        function (error, result) {
+          console.log(error, result);
+        },
+      )}
     </div>
   );
 }
