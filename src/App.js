bb7c84006dcd2a426c5fedb976a24084660d9021
Tom wallwin <tomwallwin@toms-mbp.home>
2022-03-17 15:15:36 +0000

    works for images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import cloudinary from "cloudinary";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 function App() {
-  console.log(process.env);
+  const onDrop = useCallback((files) => {
+    const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_NAME}/upload`;
+    console.log(files);
+
+    files.forEach(async (file) => {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append(
+        "upload_preset",
+        `${process.env.REACT_APP_UPLOAD_PRESET}`,
+      );
+
+      const response = await fetch(url, {
+        method: "post",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+      console.log("done");
+    });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accepts: "*",
+    multiple: false,
+  });
   return (
     <div className="App">
       <header className="App-header">Upload a photo to cloudinary</header>
-      {cloudinary.openUploadWidget(
-        {
-          cloud_name: `${process.env.REACT_APP_NAME}`,
-          upload_preset: `${process.env.REACT_APP_UPLOAD_PRESET}`,
-        },
-        function (error, result) {
-          console.log(error, result);
-        },
-      )}
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
     </div>
   );
 }
